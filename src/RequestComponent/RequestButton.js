951806f98bc3666e02645eb5f128921606b0a84f
ankitkayastha <ankit.kayastha@duke.edu>
2017-02-06 02:24:59 -0500

    approve action works

--- src/RequestComponent/RequestButton.js
+++ src/RequestComponent/RequestButton.js
@@ -1,15 +1,88 @@
 import React from "react";
+import {hashHistory} from "react-router";
+
+var xhttp = new XMLHttpRequest();
 
 class RequestButton extends React.Component {
     constructor(props){
-        super(props)
+        super(props);
+        console.log("selected is " + this.props.selected)
+        this.approveClick = this.approveClick.bind(this);
+        this.approveRequest = this.approveRequest.bind(this);
+        this.denyClick = this.denyClick.bind(this);
+        this.denyRequest = this.denyRequest.bind(this);
     }
-
+    denyRequest(requestID) {
+        var admin_comment = "this is a general admin comment to deny request";
+        var url = "https://asap-test.colab.duke.edu/api/request/deny/" + requestID + "/";
+        xhttp.open("PATCH", url, false); //synchronous request
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.setRequestHeader("Authorization", "Bearer " + localStorage.token);
+        var requestBody = {"id": requestID, "admin_comment": admin_comment};
+        //convert JSON to string to send for PATCH request
+        xhttp.send(JSON.stringify(requestBody));
+        if (xhttp.status === 401 || xhttp.status === 500){
+            if(!!localStorage.token){
+                delete localStorage.token;
+            }
+            this.setState({
+                _loginState: false
+            });
+            hashHistory.push('/login');
+            return null;
+        }
+        else {
+            var response = JSON.parse(xhttp.responseText);
+            console.log("about to print response");
+            console.log(response);
+        }
+    }
+    approveRequest(requestID) {
+        var admin_comment = "this is a general admin comment";
+        var url = "https://asap-test.colab.duke.edu/api/request/approve/" + requestID + "/";
+        xhttp.open("PATCH", url, false); //synchronous request
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.setRequestHeader("Authorization", "Bearer " + localStorage.token);
+        var requestBody = {"id": requestID, "admin_comment": admin_comment};
+        //convert JSON to string to send for PATCH request
+        xhttp.send(JSON.stringify(requestBody));
+        if (xhttp.status === 401 || xhttp.status === 500){
+            if(!!localStorage.token){
+                delete localStorage.token;
+            }
+            this.setState({
+                _loginState: false
+            });
+            hashHistory.push('/login');
+            return null;
+        }
+        else {
+            var response = JSON.parse(xhttp.responseText);
+            console.log("about to print response");
+            console.log(response);
+        }
+    }
+    approveClick() {
+        var requestIDs = this.props.selected;
+        var i;
+        for (i = 0; i < requestIDs.length; i++) {
+            console.log(requestIDs[i])
+            this.approveRequest(requestIDs[i]);
+        }
+    }
+    denyClick() {
+        var requestIDs = this.props.selected;
+        var i;
+        for (i = 0; i < requestIDs.length; i++) {
+            console.log(requestIDs[i])
+            this.denyRequest(requestIDs[i]);
+        }
+    }
     render() {
         return(
             <div>
-                <button>Approve</button>
-                <button>Deny</button>
+                <button onClick={this.approveClick}>Approve</button>
+                <button onClick={this.denyClick}>Deny</button>
             </div>
         )
     }
