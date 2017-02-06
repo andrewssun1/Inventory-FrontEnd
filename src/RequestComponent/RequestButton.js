46883ae457d8246bbdff5fc03522d0baaed6a623
ankitkayastha <ankit.kayastha@duke.edu>
2017-02-06 03:36:22 -0500

    deny works

--- src/RequestComponent/RequestButton.js
+++ src/RequestComponent/RequestButton.js
@@ -13,8 +13,10 @@ class RequestButton extends React.Component {
         this.denyRequest = this.denyRequest.bind(this);
     }
     denyRequest(requestID) {
+        //TODO the admin_comment field will be changed to come from the modal
         var admin_comment = "this is a general admin comment to deny request";
-        var url = "https://asap-test.colab.duke.edu/api/request/deny/" + requestID + "/";
+        //var url = "https://asap-test.colab.duke.edu/api/request/deny/" + requestID + "/";
+        var url = "http://localhost:8000/api/request/deny/" + requestID + "/";
         xhttp.open("PATCH", url, false); //synchronous request
         xhttp.setRequestHeader("Content-Type", "application/json");
         xhttp.setRequestHeader("Authorization", "Bearer " + localStorage.token);
@@ -22,6 +24,10 @@ class RequestButton extends React.Component {
         //convert JSON to string to send for PATCH request
         xhttp.send(JSON.stringify(requestBody));
         if (xhttp.status === 401 || xhttp.status === 500){
+            console.log("deny request did not work")
+            var response = JSON.parse(xhttp.responseText);
+            console.log("about to print response");
+            console.log(response);
             if(!!localStorage.token){
                 delete localStorage.token;
             }
@@ -38,6 +44,7 @@ class RequestButton extends React.Component {
         }
     }
     approveRequest(requestID) {
+        //TODO the admin_comment field will come from the modal
         var admin_comment = "this is a general admin comment";
         var url = "https://asap-test.colab.duke.edu/api/request/approve/" + requestID + "/";
         xhttp.open("PATCH", url, false); //synchronous request
@@ -83,6 +90,7 @@ class RequestButton extends React.Component {
             <div>
                 <button onClick={this.approveClick}>Approve</button>
                 <button onClick={this.denyClick}>Deny</button>
+                <button onClick={this.disburseClick}>Deny</button>
             </div>
         )
     }
