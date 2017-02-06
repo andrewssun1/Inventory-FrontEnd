05df28fd51550ef1eddf8301ba383c14d7dd126e
ankitkayastha <ankit.kayastha@duke.edu>
2017-02-06 17:23:14 -0500

    added cancel and stuff

--- src/RequestComponent/RequestButton.js
+++ src/RequestComponent/RequestButton.js
@@ -1,5 +1,8 @@
 import React from "react";
 import {hashHistory} from "react-router";
+import { checkAuthAndAdmin } from "../Utilities";
+var Bootstrap = require('react-bootstrap');
+var Button = Bootstrap.Button;
 
 var xhttp = new XMLHttpRequest();
 
@@ -11,6 +14,13 @@ class RequestButton extends React.Component {
         this.approveRequest = this.approveRequest.bind(this);
         this.denyClick = this.denyClick.bind(this);
         this.denyRequest = this.denyRequest.bind(this);
+        this.cancelClick = this.cancelClick.bind(this);
+        this.cancelRequest = this.cancelRequest.bind(this);
+    }
+    componentWillMount() {
+        if (checkAuthAndAdmin()) {
+            console.log("checked auth and amin");
+        }
     }
     denyRequest(requestID) {
         //TODO the admin_comment field will be changed to come from the modal
@@ -43,6 +53,39 @@ class RequestButton extends React.Component {
             console.log(response);
         }
     }
+
+    cancelRequest(requestID) {
+        //TODO the admin_comment field will be changed to come from the modal
+        var cancellationReason = "this is a general comment to cancel request";
+        //var url = "https://asap-test.colab.duke.edu/api/request/deny/" + requestID + "/";
+        var url = "http://localhost:8000/api/request/cancel/" + requestID + "/";
+        xhttp.open("PATCH", url, false); //synchronous request
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.setRequestHeader("Authorization", "Bearer " + localStorage.token);
+        var requestBody = {"id": requestID, "reason": cancellationReason};
+        //convert JSON to string to send for PATCH request
+        xhttp.send(JSON.stringify(requestBody));
+        if (xhttp.status === 401 || xhttp.status === 500){
+            console.log("cancel request did not work")
+            var response = JSON.parse(xhttp.responseText);
+            console.log("about to print response");
+            console.log(response);
+            if(!!localStorage.token){
+                delete localStorage.token;
+            }
+            this.setState({
+                _loginState: false
+            });
+            hashHistory.push('/login');
+            return null;
+        }
+        else {
+            var response = JSON.parse(xhttp.responseText);
+            console.log("about to print response");
+            console.log(response);
+        }
+    }
+
     approveRequest(requestID) {
         //TODO the admin_comment field will come from the modal
         var admin_comment = "this is a general admin comment";
@@ -65,7 +108,6 @@ class RequestButton extends React.Component {
         }
         else {
             var response = JSON.parse(xhttp.responseText);
-            window.location.reload();
             console.log("about to print response");
             console.log(response);
         }
@@ -86,13 +128,31 @@ class RequestButton extends React.Component {
             this.denyRequest(requestIDs[i]);
         }
     }
+    cancelClick() {
+        var requestIDs = this.props.selected;
+        var i;
+        for (i = 0; i < requestIDs.length; i++) {
+            console.log(requestIDs[i])
+            this.cancelRequest(requestIDs[i]);
+        }
+    }
     render() {
-
+        const isAdmin = (localStorage.isAdmin === "true");
         return(
+            isAdmin ?
             <div>
-                <button onClick={this.approveClick}>Approve</button>
-                <button onClick={this.denyClick}>Deny</button>
+                <Bootstrap.ButtonToolbar>
+                    <Button onClick={this.approveClick} bsStyle="success">Approve</Button>
+                    <Button onClick={this.denyClick} bsStyle="danger">Deny</Button>
+                </Bootstrap.ButtonToolbar>
+            </div> :
+            <div>
+                <Bootstrap.ButtonToolbar>
+                    <Button onClick={this.cancelClick} bsStyle="danger">Cancel</Button>
+                    <Button onClick={this.denyClick} bsStyle="info">New Request</Button>
+                </Bootstrap.ButtonToolbar>
             </div>
+
         )
     }
 }
